b09fd1b94db72ea57abbd19e25e5a70e923cea11
fotini <f.batzoni@gmail.com>
2023-02-17 12:58:02 +0200

    Add project files.

---
+++ PersonReader.Factory/ReaderFactory.cs
@@ -0,0 +1,28 @@
+﻿using PersonReader.CSV;
+using PersonReader.Interface;
+using PersonReader.Service;
+using PersonReader.SQL;
+
+namespace PersonReader.Factory
+{
+    public class ReaderFactory
+    {
+        public IPersonReader GetReader(string readerType)
+        {
+            switch (readerType)
+            {
+                case "Service":
+                    return new ServiceReader();
+                    break;
+                case "CSV":
+                    return new CSVReader();
+                    break;
+                case "SQL":
+                    return new SQLReader();
+                    break;
+                default: throw new ArgumentException($"Invalid Reader type: {readerType}");
+
+            }
+        }
+    }
+}
